78a3702a9edb2d4b5fc0bdc58292ec53b735c635
marahhanini <marahhanini2@gmail.com>
2024-08-04 17:50:22 +0300

    [TR/BE:FL-3445] Logging

---
+++ sensors/logging_to_console.py
@@ -0,0 +1,20 @@
+import logging
+
+# Set up logging configuration
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Get logger
+logger = logging.getLogger('my_logger')
+
+def test_logging():
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
+
+if __name__ == "__main__":
+    test_logging()
