d903fffe7f23ae4bf39280cd9ac8b5603d39f8b5
marahhanini <marahhanini2@gmail.com>
2024-08-07 23:39:32 +0300

    [TR/BE:FL-3446] Django ORM

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
